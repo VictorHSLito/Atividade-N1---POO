094df66d02b4e02fdee10246a7642117b70ce065
Victor Hugo <victorferrarihugo@hotmail.com>
2024-09-20 22:52:52 -0300

    Adição de novos métodos

--- AtividadeN1/Principal.java
+++ AtividadeN1/Principal.java
@@ -1,10 +1,5 @@
 package AtividadeN1;
 
-import java.time.DateTimeException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-
 public class Principal {
     public static void main(String[] args) {
         System.out.println("\t\tSEJA BEM-VINDO(A) AO SISTEMA DE AVALIAÇÃO DE FUNCIONÁRIOS V1.0");
