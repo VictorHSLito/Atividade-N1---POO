92c80a8a4e288fcf32287f0cc52098567bf8337b
Lukiwo0 <miguelhenrique2701@gmail.com>
2024-09-21 16:49:56 -0300

    Adicionamento de algumas coisas
    Add opção no Principal, em Funcionario na parte de Nome e Cargo proibir colocar número na palavra, em Avaliação novas validações na data.

--- Avaliacao.java
+++ Avaliacao.java
@@ -1,65 +1,135 @@
+package AtividadeN1;
+
+import java.time.LocalDate;
 import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.DateTimeException;
 
 public class Avaliacao {
     private double[] notas = new double[3];
     private double notaFinal = 0;
-    private String data1Funcionario, data2Funcionario;
+    private String[] datas = new String[3];
+
 
     Scanner sc = new Scanner(System.in);
+    DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public Avaliacao() {
         setNotas();
+        setDatas(datas);
     }
 
     private void setNotas() {
-      for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             boolean notaValida = false;
+
             while (!notaValida) {
-                System.out.printf("Digite a nota %dª nota: ", i+1);
+                System.out.printf("Digite a nota %dª nota: ", i + 1);
                 notas[i] = sc.nextDouble();
+                sc.nextLine();
                 if (notas[i] < 0 || notas[i] > 10) {
                     System.out.println("Nota inválida! Digite uma nota entre 0 e 10!");
-
-                }
-                else {
+                } else {
                     notaValida = true;
                     notaFinal += notas[i];
                 }
             }
         }
-        notaFinal = notaFinal/3;
+        notaFinal = notaFinal / 3;
     }
 
     public void getNotafinal() {
         if (notaFinal <= 3) {
             System.out.printf("Nota Final: %.2f\nDesempenho final: Ruim!", notaFinal);
-
         } else if (notaFinal <= 4) {
             System.out.printf("Nota Final: %.2f\nDesempenho final: Regular!", notaFinal);
-
         } else if (notaFinal <= 7) {
             System.out.printf("Nota Final: %.2f\nDesempenho final: Bom!", notaFinal);
-
         } else {
             System.out.printf("Nota Final: %.2f\nDesempenho final: Excelente!", notaFinal);
-
         }
     }
 
-    public String getData1Funcionario() {
-        return data1Funcionario;
+    public void getNotas() {
+        int opc;
+        do {
+            System.out.println("\nQual nota gostaria de ver?\n1 - 1ª Nota\n2 - 2ª Nota\n3 - 3ª Nota");
+            System.out.print("Escolha uma opção: ");
+            opc = sc.nextInt();
+            sc.nextLine(); // Limpa o buffer do scanner
+            switch (opc) {
+                case 1:
+                    System.out.println("Nota 1: " + this.notas[0]);
+                    break;
+                case 2:
+                    System.out.println("Nota 2: " + this.notas[1]);
+                    break;
+                case 3:
+                    System.out.println("Nota 3: " + this.notas[2]);
+                    break;
+                default:
+                    System.out.println("Não foi selecionada uma opção válida!");
+                    break;
+            }
+        } while (opc < 1 || opc > 3);
     }
 
-    public void setData1Funcionario(String data1Funcionario) {
-        this.data1Funcionario = data1Funcionario;
+    public void getDatas() {
+        int opc;
+        do {
+            System.out.println("\nQual data gostaria de ver?\n1 - 1ª Data\n2 - 2ª Data\n3 - 3ª Data");
+            System.out.print("Escolha uma opção: ");
+            opc = sc.nextInt();
+            sc.nextLine(); // Limpa o buffer do scanner
+            switch (opc) {
+                case 1:
+                    System.out.println("Data 1: " + this.datas[0]);
+                    break;
+                case 2:
+                    System.out.println("Data 2: " + this.datas[1]);
+                    break;
+                case 3:
+                    System.out.println("Data 3: " + this.datas[2]);
+                    break;
+                default:
+                    System.out.println("Não foi selecionada uma opção válida!");
+                    break;
+            }
+        } while (opc < 1 || opc > 3);
     }
 
-    public String getData2Funcionario() {
-        return data2Funcionario;
+    public void setDatas(String[] datas) {
+        datas[0] = lerData(1, null, null);
+        datas[1] = lerData(2, datas[0], null);
+        datas[2] = lerData(3, datas[0], datas[1]);
     }
 
-    public void setData2Funcionario(String data2Funcionario) {
-        this.data2Funcionario = data2Funcionario;
-    }
+    private String lerData(int index, String dataPassada01, String dataPassada02) {
+        //System.out.printf("Digite a data da avaliação do %dº trimestre: ", index);
 
+        while (true) {
+            System.out.printf("Digite a data da avaliação do %dº trimestre: ", index);
+            String dataEntrada = sc.nextLine();
+
+            try {
+                LocalDate dataInserida = LocalDate.parse(dataEntrada, formatador);
+
+                if (dataInserida.isAfter(LocalDate.now())) {
+                    System.out.println("Data inválida! A data não pode ser no futuro.");
+                }  else if (dataPassada01 != null && dataInserida.isEqual(LocalDate.parse(dataPassada01, formatador)) ||
+                        dataPassada01 != null && dataInserida.isBefore(LocalDate.parse(dataPassada01, formatador)) ||
+                        dataPassada02 != null && dataInserida.isEqual(LocalDate.parse(dataPassada02, formatador)) ||
+                        dataPassada02 != null && dataInserida.isBefore(LocalDate.parse(dataPassada02, formatador))) {
+                    System.out.println("Data inválida! A data não pode ser igual as anterior" +
+                            "\nE a data do 2º trimestre não pode ser antes da data do 1º trimestre." +
+                            "\nE a data do 3º trimestre não pode ser antes da data do 1º e 2º trimestre.!");
+                } else {
+                    return dataInserida.format(formatador);
+                }
+            } catch (DateTimeException e) {
+                System.out.println("Data inválida! Exemplo de Data: 17/09/2024");
+            }
+
+        }
+    }
 }
--- Funcionario.java
+++ Funcionario.java
@@ -1,4 +1,4 @@
-import com.sun.management.UnixOperatingSystemMXBean;
+package AtividadeN1;
 
 import java.util.Scanner;
 
@@ -6,22 +6,55 @@ public class Funcionario {
     private String nome;
     private String cargo;
     private float salario;
+    private Avaliacao avaliacao;
 
     Scanner sc = new Scanner(System.in);
 
     public Funcionario () {
+        System.out.println("\n\tDigite os dados do funcionário(a) abaixo\n");
         setNome();
         setCargo();
         setSalario();
+        this.avaliacao = new Avaliacao();
     }
 
+    public void details() {
+        System.out.println("\t-----------DADOS DO FUNCIONÁRIO(A)-----------");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Cargo: " + getCargo());
+        System.out.println("Salário: " + getSalario());
+        System.out.println("\t-----------AVALIAÇÃO FINAL DO FUNCIONÁRIO(A)-----------");
+        avaliacao.getNotafinal();
+    }
     public String getNome() {
         return nome;
     }
 
     private void setNome() {
-        System.out.println("Qual o nome do funcionário? ");
-        this.nome = sc.next();
+        boolean sair = false;
+        do {
+            int j = 0;
+            System.out.println("Qual o nome do funcionário(a)? ");
+            this.nome = sc.nextLine();
+
+            for (int i = 0; i < this.nome.length(); i++) {
+                if (Character.isDigit(this.nome.charAt(i))) {
+                    j++;
+                    break;
+                } else {
+                    j = 0;
+                }
+            }
+
+            if (this.nome.length() <= 2) {
+                System.out.println("AVISO \nO nome precisa ter mais de 2 caracteres.");
+            } else if (j >= 1) {
+                System.out.println("AVISO \nO nome não pode ter número.");
+            } else {
+                sair = true;
+            }
+
+        } while (sair != true);
     }
 
     public String getCargo() {
@@ -29,8 +62,30 @@ public class Funcionario {
     }
 
     private void setCargo() {
-        System.out.println("Qual o cargo do funcionário, " + getNome() + "?");
-        this.cargo = sc.next();
+        boolean sair = false;
+        do {
+            int j = 0;
+            System.out.println("Qual o cargo de " + getNome() + "?");
+            this.cargo = sc.nextLine();
+
+            for (int i = 0; i < this.cargo.length(); i++) {
+                if (Character.isDigit(this.cargo.charAt(i))) {
+                    j++;
+                    break;
+                } else {
+                    j = 0;
+                }
+            }
+
+            if (this.cargo.length() <= 2) {
+                System.out.println("AVISO \nO cargo precisa ter mais de 2 caracteres.");
+            } else if (j >= 1) {
+                System.out.println("AVISO \nO cargo não pode ter número.");
+            } else {
+                sair = true;
+            }
+
+        } while (sair != true);
     }
 
     public float getSalario() {
@@ -38,7 +93,53 @@ public class Funcionario {
     }
 
     private void setSalario() {
-        System.out.println("Qual o salário de " + getNome() + "?");
-        this.salario = sc.nextFloat();
+        while (true) {
+            System.out.println("Qual o salário de " + getNome() + "?");
+            this.salario = sc.nextFloat();
+            if (this.salario > 1411.99) {
+                break;
+            } else {
+                System.out.println("AVISO \nO salário não pode ser menor que um salário mínimo (R$ 1.412,00).");
+            }
+        }
+    }
+
+    public void getAvaliacao() {
+        avaliacao.getNotas();
+        avaliacao.getDatas();
+    }
+
+    public void mudarInformacoes() {
+        int opc;
+        do {
+            System.out.println("\nGostaria de mudar qual informação do funcionário?");
+            System.out.println("1 - Nome\n2 - Cargo\n3 - Salário\n4 - Tudo");
+            opc = sc.nextInt();
+            sc.nextLine(); // Limpa o buffer do scanner
+            switch (opc) {
+                case 1:
+                    System.out.println("Digite o novo nome abaixo: ");
+                    setNome();
+                    break;
+                case 2:
+                    System.out.println("Digite o novo cargo abaixo: ");
+                    setCargo();
+                    break;
+                case 3:
+                    System.out.println("Digite o novo salário abaixo: ");
+                    setSalario();
+                    break;
+                case 4:
+                    System.out.println("Digite as novas informações do funcionário abaixo: ");
+                    setNome();
+                    setCargo();
+                    setSalario();
+                    this.avaliacao = new Avaliacao();
+                    break;
+                default:
+                    System.out.println("Não foi selecionada uma opção válida!");
+                    break;
+            }
+        } while (opc < 1 || opc > 4);
     }
 }
