71ad1111045ccecf74be994c26e84286044b2975
Victor Hugo <victorferrarihugo@hotmail.com>
2024-09-21 23:57:52 -0300

    Descrição

--- AtividadeN1/Principal.java
+++ AtividadeN1/Principal.java
@@ -7,6 +7,9 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         int qtd;
         System.out.println("\t\tSEJA BEM-VINDO(A) AO SISTEMA DE AVALIAÇÃO DE FUNCIONÁRIOS V1.0");
+        System.out.println("\n\tSistema de Avaliação: Durante o ano, você será avaliado em três trimestres,\n" +
+                "\trecebendo uma nota de 0 a 10 em cada um. Ao final, será somado as três notas \n\te dividas por 3" +
+                " para calcular a nota final.\n");
         do {
             System.out.println("Gostaria de cadastrar quantos funcionários? ");
             qtd = sc.nextInt();
